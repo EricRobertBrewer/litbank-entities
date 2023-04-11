1073730ada223584628e2253286992a8e04ba0db
Eric Brewer <eric.r.brewer@gmail.com>
2023-04-11 10:32:35 -0600

    Fix bert run script.

--- bert.sh
+++ bert.sh
@@ -2,7 +2,7 @@
 
 for CATEGORY in PER FAC GPE LOC VEH ORG
 do
-  for i in 1 .. 10
+  for ((i = 1; i <= 10; i++))
   do
     python -m litbank_entities.extract bert --folds 10 --fold $i --categories $CATEGORY
   done
